742758c8397b42491348c2e1e6247e6d3ef38982
varadasainikhil <varadasainikhil@gmail.com>
2025-07-23 09:48:48 -0400

    Initial Commit

---
+++ SwiftfulMapApp/Models/location.swift
@@ -0,0 +1,18 @@
+//
+//  location.swift
+//  SwiftfulMapApp
+//
+//  Created by Sai Nikhil Varada on 7/22/25.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    var name : String
+    var cityName : String
+    var coordinates : CLLocationCoordinate2D
+    var description : String
+    var imageNames : [String]
+    var link : String
+}
---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Sai Nikhil Varada on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
